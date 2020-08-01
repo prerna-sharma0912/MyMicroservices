0fba66d2dc9da433409fb7d0f6470352d113fbef
prerna-sharma0912 <64975505+prerna-sharma0912@users.noreply.github.com>
2020-08-01 12:54:16 +0000

    Update LoginFunctionalTest.java

--- ILP_BookStoreWorkspace/src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
+++ ILP_BookStoreWorkspace/src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
@@ -31,7 +31,7 @@ public class LoginFunctionalTest {
 		// new FirefoxDriver();
 				FirefoxBinary firefoxBinary = new FirefoxBinary();
         firefoxBinary.addCommandLineOptions("--headless");
-        System.setProperty("webdriver.gecko.driver", "/usr/local/bin");
+        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setBinary(firefoxBinary);
         
